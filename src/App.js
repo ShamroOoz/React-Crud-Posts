1e1cd132a13f6c6f60bdee5a98cf3870caf45b0e
shamroz73 <shamrozwarraich@gmail.com>
2022-03-12 16:40:40 +0100

    basic route change

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Layout from "Pages/Layout";
+import Home from "Components/Home";
+import About from "Components/About";
+import Contact from "Components/Contact";
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Layout />}></Route>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="contact" element={<Contact />} />
+      </Route>
     </Routes>
   );
 };
--- src/Components/Contatc.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Contatc = () => {
-  return <div>Contatc</div>;
-};
-
-export default Contatc;
